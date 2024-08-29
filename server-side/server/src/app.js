3c59c1173fa86948e4c90143a31a96f1f105967c
shubham-goyal-0811 <shubhamsubbi1@gmail.com>
2024-08-29 17:43:21 +0530

    Utils and database added

---
+++ server-side/server/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import cookieparser from 'cookie-parser'
+
+const app = express();
+app.use(cors({
+    origin : process.env.origin,
+    credentials : true
+}))
+
+app.use(express.json({limit : "16kb"}))//for data coming from express files
+app.use(express.urlencoded({extended:true,limit:"16kb"}));//for data coming from Url
+app.use(express.static("public")) //to store some assets publically
+app.use(cookieparser());//used to access cookies of the browser
+
+
+
+export {app};
